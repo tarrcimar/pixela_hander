1e608d9b2ce6db5b33a2d006f5a8cb4419aac1e1
Tarr Márton <tarr.marton1@gmail.com>
2021-03-17 19:59:16 +0100

    "Base functionality done"

--- pixela_manager.py
+++ pixela_manager.py
@@ -1,10 +1,13 @@
 import requests
 import datetime
 import sys
-from tkinter import *
-from PIL import ImageTk, Image
 import webbrowser
 import time
+import os
+import random
+
+from tkinter import *
+from PIL import ImageTk, Image
 
 USERNAME = ""
 TOKEN = ""
@@ -13,9 +16,9 @@ GRAPHID = ""
 ARGS = sys.argv
 LENGTH = len(ARGS)
 
-ORIGINAL_ENDPOINT = 'https://pixe.la/v1/users'
+ORIGINAL_ENDPOINT = 'https://pixe.la/v1/users' # api endpoint
 
-COLORS = {
+COLORS = { # different colors and their representation in the parameters
     "shibafu":"green",
     "momiji":"red",
     "sora":"purple",
@@ -26,12 +29,11 @@ COLORS = {
 
 FONT = ("Corbel", 12, "bold")
 color = ""
-OPTIONS = ['Update','New', 'Delete']
+OPTIONS = ['update_pixel','New', 'delete_pixel']
 
 headers = {
-    "X-USER-TOKEN":"skljfva8a4wrm283"
+    "X-USER-TOKEN":TOKEN
 }
-
 
 date = datetime.datetime.now()
 TODAY = date.strftime(r"%Y%m%d")
@@ -44,24 +46,30 @@ BG_ALT = "#276722"
 SUCCESS = "#42f557"
 FAILURE = "#e85423"
 
-def get_logo_color():
+def submit():
+    if(list.get() == "New"):
+        add_pixel()
+    elif(list.get() == "update_pixel"):
+        update_pixel()
+    elif(list.get() == "delete_pixel"):
+        delete_pixel()
+
+
+def resetlabel():
+    status_label.config(state=DISABLED, fg="#000000")
+
+
+def get_logo_color(graph_no): # get the logo color according to the graph color
     graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs"
     print(USERNAME, TOKEN)
     response = requests.get(url=graph_endpoint, headers=headers)
     response = response.json()
-    color = COLORS[response["graphs"][0]["color"]]
+    color = COLORS[response["graphs"][graph_no]["color"]]
 
     return color
 
-def submit():
-    if(list.get() == "New"):
-        add()
-    elif(list.get() == "Update"):
-        update()
-    elif(list.get() == "Delete"):
-        delete()
 
-def increment():
+def increment_pixel(): # add_pixel +1 or +0.1 to the pixel
     graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}/increment"
 
     response = requests.put(url=graph_endpoint, headers=headers)
@@ -72,7 +80,8 @@ def increment():
     window.after(3000, func=resetlabel)
     print(response.text)
 
-def decrement():
+
+def decrement_pixel(): # subtract +1 or -0.1 from the pixel
     graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}/decrement"
 
     response = requests.put(url=graph_endpoint, headers=headers)
@@ -83,11 +92,14 @@ def decrement():
     window.after(3000, func=resetlabel)
     print(response.text)
 
-def resetlabel():
-    status_label.config(state=DISABLED)
     
 #NEW USER
-def new_user(username, token):
+def new_user(username, token): # create_graph the new user, delete_pixel the old text
+    try:
+        os.remove("user.txt")
+    except FileNotFoundError:
+        print("User not in file. Creating...")
+    
     user_params = {
         "token":token,
         "username":username,
@@ -95,17 +107,39 @@ def new_user(username, token):
         "notMinor":"yes"
     }
 
-    response = requests.post(url=ORIGINAL_ENDPOINT, json=user_params) #--> user created, don't need it anymore
+    response = requests.post(url=ORIGINAL_ENDPOINT, json=user_params)
     print(response.text)
-    with open("user.txt", "x") as file:
-        file.write(f"{USERNAME},{TOKEN}")
+    with open("user.txt", "w") as file:
+        file.write(f"{username},{token}")
 
+def delete_user():
+    print("This action will delete the user")
+    username = input("Username: ")
+    token = input("Token: ")
 
-## CREATE GRAPH
-def create(graph_id, graph_name, graph_unit, graph_type, graph_color):
+    delete_endpoint = graph_endpoint = f"{ORIGINAL_ENDPOINT}/{username}"
+    headers['X-USER-TOKEN'] = token
+
+    response = requests.delete(url=delete_endpoint, headers=headers)
+    print(response.text)
+
+def create_graph(): # create a new graph
+    global TOKEN
+
+    print("Please specify your new graph.") #Name of the graph: Unit of graph: Type of graph: Color of pixels:
+    graph_id = input("ID of the graph: ")
+    graph_name = input("Graph Name: ")
+    graph_unit = input("Unit of graph: ")
+    graph_type = input("Type of graph (int/float): ")
+    print("Available colors: shibafu (green), momiji (red), sora (blue), ichou (yellow), ajisai (purple) and kuro (black)")
+    graph_color = input("Color of pixels: ")
+    read_user()
+    headers["X-USER-TOKEN"] = TOKEN
+    print(USERNAME, headers)
+
     graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs"
     
-    create_params = {
+    create_graph_params = {
         "id":graph_id,
         "name":graph_name,
         "unit":graph_unit,
@@ -113,31 +147,15 @@ def create(graph_id, graph_name, graph_unit, graph_type, graph_color):
         "color":graph_color
     }
 
-    print(create_params)
-
-    response = requests.post(url=graph_endpoint, json=create_params, headers=headers)
+    response = requests.post(url=graph_endpoint, json=create_graph_params, headers=headers)
+    print(response.text)
+    print(response.status_code)
     with open("user.txt", "a") as file:
         file.write(f",{graph_id}")
 
-retek = []
 
-def read_user():
-    global retek
-    global USERNAME
-    global TOKEN
-    global GRAPHID
-
-    with open("user.txt", "r") as file:
-        retek = file.readlines()
-    retek = retek[0].split(",")
-    USERNAME = retek[0]
-    TOKEN = retek[1]
-    GRAPHID = retek[2]
-    print(USERNAME,TOKEN,GRAPHID)
-
-
-##ADD PIXEL
-def add():
+#add_pixel PIXEL
+def add_pixel():
     pixeladd_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}"
 
     if(len(date_input.get()) >= 1):
@@ -152,14 +170,14 @@ def add():
 
     response = requests.post(url=pixeladd_endpoint, json=pixel_config, headers=headers)
     if(response.status_code != 200):
-        status_label.config(state=NORMAL, text="Adding failed")
+        status_label.config(state=NORMAL, text="add_pixeling failed. Check date and value.", fg="#ff0000")
     else:
-        status_label.config(state=NORMAL, text="Pixel added")
+        status_label.config(state=NORMAL, text="Pixel add_pixeled", fg="#000000")
     print(response.text)
     window.after(3000, func=resetlabel)
 
-##UPDATE A PIXEL
-def update():
+
+def update_pixel(): # update_pixel a pixels value
     if(len(date_input.get()) >= 1):
         commit_time = date_input.get()
     else:
@@ -167,70 +185,93 @@ def update():
     
     print(commit_time)
 
-    update_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}/{commit_time}"
+    update_pixel_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}/{commit_time}"
 
-    update_config = {
+    update_pixel_config = {
         "quantity":f"{counter_input.get()}"
     }
 
-    response = requests.put(url=update_endpoint, json=update_config, headers=headers)
+    response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
     if(response.status_code != 200):
-        status_label.config(state=NORMAL, text="Update unsuccessful")
+        status_label.config(state=NORMAL, text="update_pixel unsuccessful. Check date format.", fg="#ff0000")
     else:
-        status_label.config(state=NORMAL, text="Update successful")
+        status_label.config(state=NORMAL, text="update_pixel successful", fg="#000000")
     window.after(3000, func=resetlabel)
     print(response.text)
 
-#DELETE PIXEL
-def delete():
+
+def delete_pixel(): # delete_pixel a pixel
+    if(len(date_input.get()) >= 1): # check if there's a date specified
+        commit_time = date_input.get()
+    else:
+        commit_time = TODAY
     graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}/{commit_time}"
 
-    response = requests.delete(url=graph_endpoint, headers=headers)
+    response = requests.delete_pixel(url=graph_endpoint, headers=headers)
     if(response.status_code != 200):
-        status_label.config(state=NORMAL, text="Delete unsuccessful")
+        status_label.config(state=NORMAL, text="delete_pixel unsuccessful. Check date format.", fg="#ff0000")
     else:
-        status_label.config(state=NORMAL, text="Pixel deleted")
+        status_label.config(state=NORMAL, text="Pixel delete_pixeld", fg="#000000")
     window.after(3000, func=resetlabel)
     print(response.text)
+
+
+userdata = []
+
+def read_user():
+    global userdata
+    global USERNAME
+    global TOKEN
+    global GRAPHID
+
+    with open("user.txt", "r") as file:
+        userdata = file.readlines()
+    userdata = userdata[0].split(",")
+    USERNAME = userdata[0]
+    TOKEN = userdata[1]
+    print(USERNAME, TOKEN)
+    if(len(userdata)>2):
+        GRAPHID = userdata[2]
+
 
 answer = ""
 
+#chech cl arguments
 if(LENGTH>1):
     if(ARGS[1] == "register"): # register username token
         username = input("Enter a username: ")
-        token = input("Enter a token: ")
-        read_user()
+        token = input("Enter a token (8 characters minimum): ")
+        new_user(username, token)
 
         answer = input("Do you want to create a graph? Y/N: ")
         if(answer == 'Y'):
-            create()
-    elif(ARGS[1] == "create"):
-        print("Please specify your new graph.") #Name of the graph: Unit of graph: Type of graph: Color of pixels:
-        graph_id = input("ID of the graph: ")
-        graph_name = input("Graph Name:")
-        graph_unit = input("Unit of graph: ")
-        graph_type = input("Type of graph (int/float): ")
-        print("Available colors: shibafu (green), momiji (red), sora (blue), ichou (yellow), ajisai (purple) and kuro (black)")
-        graph_color = input("Color of pixels: ")
-        print(USERNAME)
-        create(graph_id, graph_name, graph_unit, graph_type, graph_color)
+            create_graph()
+    elif(ARGS[1] == "create_graph"):
+        create_graph()
+    elif(ARGS[1] == "delete"):
+        delete_user()
 else:
     read_user()
     headers["X-USER-TOKEN"] = TOKEN
 
-    print(f"Available graphs: {retek[2:]}")
-    graph = input("Which graph?")
+    # get the available graphs from the textfile
+    print(f"Available graphs: {userdata[2:]}")
+    graph = input("Choose a graph to display: ")
     GRAPHID = graph
 
+    # open the web interface
     openstring = f'{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}.html'
     webbrowser.open(openstring)
 
-    color = get_logo_color()
+    # set the logo color
+    color = get_logo_color(userdata.index(graph) -2)
     png_path = f".//images//{color}.png"
 
+
+    # create_graph the Tkinter window and the widgets
     window = Tk()
     window.config(padx = 20, pady = 20, bg=BG)
-    window.title("Pixela Poster")
+    window.title("Pixela Manager")
 
     logo = Image.open(png_path)
     logo = ImageTk.PhotoImage(logo)
@@ -262,12 +303,12 @@ else:
     button = Button(text="Submit", command = submit, fg=FG, font=FONT)
     button.grid(column = 1, row = 6, pady = 10)
 
-    increment_button = Button(text="+", command=increment, fg=FG, font=FONT)
+    increment_button = Button(text="+", command=increment_pixel, fg=FG, font=FONT)
     increment_button.grid(column=1, row = 5, pady = 10, ipadx=5, sticky = "w", padx = 30)
-    decrement_button = Button(text="-", command=decrement, fg=FG, font=FONT)
+    decrement_button = Button(text="-", command=decrement_pixel, fg=FG, font=FONT)
     decrement_button.grid(column=1, row=5, pady=10, ipadx=5,sticky="e", padx = 30)
 
-    status_label = Label(text="Something", bg=BG, fg=FG, font=FONT, disabledforeground=BG, state=DISABLED)
+    status_label = Label(text="", bg=BG, fg=FG, font=FONT, disabledforeground=BG, state=DISABLED)
     status_label.grid(column=1, row = 7)
 
 
