85d355d4330c97548b68a2933ec90b91a2610342
Tarr Márton <tarr.marton1@gmail.com>
2021-03-17 16:53:11 +0100

    "Added functionality"

--- pixela_manager.py
+++ pixela_manager.py
@@ -6,21 +6,37 @@ from PIL import ImageTk, Image
 import webbrowser
 import time
 
-USERNAME = "tmarton"
-TOKEN = "skljfva8a4wrm283"
-GRAPHID = "graph1"
+USERNAME = ""
+TOKEN = ""
+GRAPHID = ""
 
 ARGS = sys.argv
 LENGTH = len(ARGS)
 
 ORIGINAL_ENDPOINT = 'https://pixe.la/v1/users'
 
-openstring = f'{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}.html'
-webbrowser.open(openstring)
+COLORS = {
+    "shibafu":"green",
+    "momiji":"red",
+    "sora":"purple",
+    "ichou":"yellow",
+    "ajisai":"purple",
+    "kuro":"orange"
+}
+
+FONT = ("Corbel", 12, "bold")
+color = ""
+OPTIONS = ['Update','New', 'Delete']
 
 headers = {
     "X-USER-TOKEN":TOKEN
 }
+
+
+date = datetime.datetime.now()
+TODAY = date.strftime(r"%Y%m%d")
+
+commit_time = TODAY
 
 BG = "#ffffff"
 FG = "#000000"
@@ -28,13 +44,14 @@ BG_ALT = "#276722"
 SUCCESS = "#42f557"
 FAILURE = "#e85423"
 
-date = datetime.datetime.now()
-TODAY = date.strftime(r"%Y%m%d")
+def get_logo_color():
+    graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs"
 
-FONT = ("Corbel", 12, "bold")
-
-OPTIONS = ['Update','New', 'Delete']
-
+    response = requests.get(url=graph_endpoint, headers=headers)
+    response = response.json()
+    print(response)
+    color = COLORS[response["graphs"][0]["color"]]
+    return color
 
 def submit():
     if(list.get() == "New"):
@@ -52,7 +69,7 @@ def increment():
         status_label.config(state=NORMAL, text="Increment failed")
     else:
         status_label.config(state=NORMAL, text="Pixel incremented")
-    window.after(1500, func=resetlabel)
+    window.after(3000, func=resetlabel)
     print(response.text)
 
 def decrement():
@@ -63,44 +80,59 @@ def decrement():
         status_label.config(state=NORMAL, text="Decrement failed")
     else:
         status_label.config(state=NORMAL, text="Pixel decremented")
-    window.after(1500, func=resetlabel)
+    window.after(3000, func=resetlabel)
     print(response.text)
 
 def resetlabel():
     status_label.config(state=DISABLED)
     
 #NEW USER
-def new_user():
+def new_user(username, token):
     user_params = {
-        "token":TOKEN,
-        "username":USERNAME,
+        "token":token,
+        "username":username,
         "agreeTermsOfService":"yes",
         "notMinor":"yes"
     }
 
     response = requests.post(url=ORIGINAL_ENDPOINT, json=user_params) #--> user created, don't need it anymore
     print(response.text)
+    with open("user.txt", "x") as file:
+        file.write(f"{USERNAME},{TOKEN}")
 
 #new_user()
 
 ## CREATE GRAPH
-def create():
+def create(graph_id, graph_name, graph_unit, graph_type, graph_color):
     graph_endpoint = f"{ORIGINAL_ENDPOINT}/{USERNAME}/graphs"
 
     graph_config = {
-        "id":GRAPHID,
-        "name":"Daily Activity Level",
-        "unit":"commit",
-        "type":"int",
-        "color":"ichou"
+        "id":graph_id,
+        "name":graph_name,
+        "unit":graph_unit,
+        "type":graph_type,
+        "color":graph_color
     }
 
     response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
     print(response.text)
-    
-#create()
+    with open("user.txt", "a") as file:
+        file.write(f",{graph_id}")
 
-commit_time = TODAY
+retek = []
+
+def read_user():
+    global USERNAME
+    global TOKEN
+    global GRAPHID
+
+    with open("user.txt", "r") as file:
+        retek = file.readlines()
+    retek = retek[0].split(",")
+    USERNAME = retek[0]
+    TOKEN = retek[1]
+    GRAPHID = retek[2]
+    print(USERNAME,TOKEN,GRAPHID)
 
 
 ##ADD PIXEL
@@ -123,9 +155,7 @@ def add():
     else:
         status_label.config(state=NORMAL, text="Pixel added")
     print(response.text)
-    window.after(1500, func=resetlabel)
-
-
+    window.after(3000, func=resetlabel)
 
 ##UPDATE A PIXEL
 def update():
@@ -147,7 +177,7 @@ def update():
         status_label.config(state=NORMAL, text="Update unsuccessful")
     else:
         status_label.config(state=NORMAL, text="Update successful")
-    window.after(1500, func=resetlabel)
+    window.after(3000, func=resetlabel)
     print(response.text)
 
 #DELETE PIXEL
@@ -159,50 +189,84 @@ def delete():
         status_label.config(state=NORMAL, text="Delete unsuccessful")
     else:
         status_label.config(state=NORMAL, text="Pixel deleted")
-    window.after(1500, func=resetlabel)
+    window.after(3000, func=resetlabel)
     print(response.text)
 
-window = Tk()
-window.config(padx = 20, pady = 20, bg=BG)
-window.title("Pixela Poster")
+answer = ""
 
-logo = Image.open(r'./images/PIXELA_GREEN.png')
-logo = ImageTk.PhotoImage(logo)
-logolabel = Label(image=logo, height=100, width = 300, bg=BG)
-logolabel.image = logo
-logolabel.grid(column=1, row = 0)
+if(LENGTH>1):
+    if(ARGS[1] == "register"): # register username token
+        username = input("Enter a username: ")
+        token = input("Enter a token: ")
+        read_user()
 
-date_label = Label(text = "Date (YYYYMMDD)", bg=BG, fg=FG, font=FONT)
-date_label.grid(column = 1, row = 1, pady = 10)
+        answer = input("Do you want to create a graph? Y/N: ")
+        if(answer == 'Y'):
+            pass
+            #create()
+        elif(answer == 'N'):
+            pass
+    elif(ARGS[1] == "create"):
+        print("Please specify your new graph.") #Name of the graph: Unit of graph: Type of graph: Color of pixels:
+        graph_id = input("ID of the graph: ")
+        name = input("Graph Name:")
+        unit = input("Unit of graph: ")
+        type = input("Type of graph (int/float): ")
+        print("Available colors: shibafu (green), momiji (red), sora (blue), ichou (yellow), ajisai (purple) and kuro (black)")
+        pixel_color = input("Color of pixels: ")
+        create(graph_id, name, unit, type, pixel_color)
 
-date_input = Entry(width = 30, justify = CENTER)
-date_input.grid(column = 1, row = 2)
+else:
+    read_user()
+    print(USERNAME, TOKEN, GRAPHID)
+    openstring = f'{ORIGINAL_ENDPOINT}/{USERNAME}/graphs/{GRAPHID}.html'
+    #webbrowser.open(openstring)
 
-counter_label = Label(text = "Value", bg=BG, fg=FG, font=FONT)
-counter_label.grid(column = 1, row = 3, pady = 10)
+    color = get_logo_color()
+    png_path = f".//images//{color}.png"
+    print(png_path)
 
-counter_input = Entry(width = 30, justify=CENTER)
-counter_input.grid(column = 1, row = 4)
+    window = Tk()
+    window.config(padx = 20, pady = 20, bg=BG)
+    window.title("Pixela Poster")
+
+    logo = Image.open(png_path)
+    logo = ImageTk.PhotoImage(logo)
+    logolabel = Label(image=logo, height=100, width = 300, bg=BG)
+    logolabel.image = logo
+    logolabel.grid(column=1, row = 0)
+
+    date_label = Label(text = "Date (YYYYMMDD)", bg=BG, fg=FG, font=FONT)
+    date_label.grid(column = 1, row = 1, pady = 10)
+
+    date_input = Entry(width = 30, justify = CENTER)
+    date_input.grid(column = 1, row = 2)
+
+    counter_label = Label(text = "Value", bg=BG, fg=FG, font=FONT)
+    counter_label.grid(column = 1, row = 3, pady = 10)
+
+    counter_input = Entry(width = 30, justify=CENTER)
+    counter_input.grid(column = 1, row = 4)
 
 
-list = StringVar()
-list.set("Choose action")
-menu = OptionMenu(window, list, *OPTIONS)
-menu.grid(column = 1, row = 5, pady = 10)
-menu.config( fg = FG)
-menu["menu"].config(fg=FG, borderwidth=0)
-menu["highlightthickness"] = 0
+    list = StringVar()
+    list.set("Choose action")
+    menu = OptionMenu(window, list, *OPTIONS)
+    menu.grid(column = 1, row = 5, pady = 10)
+    menu.config( fg = FG)
+    menu["menu"].config(fg=FG, borderwidth=0)
+    menu["highlightthickness"] = 0
 
-button = Button(text="Submit", command = submit, fg=FG, font=FONT)
-button.grid(column = 1, row = 6, pady = 10)
+    button = Button(text="Submit", command = submit, fg=FG, font=FONT)
+    button.grid(column = 1, row = 6, pady = 10)
 
-increment_button = Button(text="+", command=increment, fg=FG, font=FONT)
-increment_button.grid(column=1, row = 5, pady = 10, ipadx=5, sticky = "w", padx = 30)
-decrement_button = Button(text="-", command=decrement, fg=FG, font=FONT)
-decrement_button.grid(column=1, row=5, pady=10, ipadx=5,sticky="e", padx = 30)
+    increment_button = Button(text="+", command=increment, fg=FG, font=FONT)
+    increment_button.grid(column=1, row = 5, pady = 10, ipadx=5, sticky = "w", padx = 30)
+    decrement_button = Button(text="-", command=decrement, fg=FG, font=FONT)
+    decrement_button.grid(column=1, row=5, pady=10, ipadx=5,sticky="e", padx = 30)
 
-status_label = Label(text="Something", bg=BG, fg=FG, font=FONT, disabledforeground=BG, state=DISABLED)
-status_label.grid(column=1, row = 7)
+    status_label = Label(text="Something", bg=BG, fg=FG, font=FONT, disabledforeground=BG, state=DISABLED)
+    status_label.grid(column=1, row = 7)
 
 
-window.mainloop()
+    window.mainloop()
